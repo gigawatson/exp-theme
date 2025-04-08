d189456caac06652e161f57b582a67bd08b342af
Exploriter <67083853+gigawatson@users.noreply.github.com>
2025-04-08 10:22:17 -0700

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header">
+        <h1 class="entry-title">Not Found</h1>
+    </header>
+    <div class="entry-content">
+        <p>Sorry, but the page you were trying to view does not exist, yet. Recalibrate your time machine, or try
+            a search instead.</p>
+        <?php get_search_form(); ?>
+    </div>
+</article>
 <?php get_footer(); ?>
