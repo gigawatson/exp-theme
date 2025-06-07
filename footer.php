dc841f664d10ba3c1f6fba12294e812447fea63e
Exploriter <67083853+gigawatson@users.noreply.github.com>
2025-06-07 09:51:49 -0700

    add header and footer elements

--- footer.php
+++ footer.php
@@ -3,12 +3,17 @@
     <?php wp_nav_menu([
         'theme_location' => 'footer-menu',
         'container' => 'nav',
+        'container_class' => '',
         'menu_class' => 'flex gap-x-4',
+        'add_li_class' => '',
+        'link_before' => '',
+        'link_after' => '',
     ]); ?>
     <div id="copyright">
         &copy; <?= esc_html(date_i18n(__('Y'))); ?> <?= esc_html(get_bloginfo('name')); ?>
     </div>
 </footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -8,16 +8,27 @@
 
 <body <?php body_class(['bg-stone-50']); ?>>
 <?php wp_body_open(); ?>
-<header>
-    <?php wp_nav_menu([
-        'theme_location' => 'main-menu',
-        'container' => 'nav',
-        'container_class' => 'w-full',
-        'menu_class' => 'flex justify-center gap-x-4',
-        'add_li_class' => '',
-    ]); ?>
-    <div id="search">
-        <?php get_search_form(); ?>
-    </div>
-</header>
-<main id="content">
+<div id="wrapper">
+    <header>
+        <div id="site-title">
+            <a href="<?= esc_url(home_url('/')) ?>" title="<?= esc_attr(get_bloginfo('name')) ?>">
+                <?= esc_html(get_bloginfo('name')) ?>
+            </a>
+        </div>
+        <div id="site-description">
+            <?= esc_html(get_bloginfo('description')) ?>
+        </div>
+        <?php wp_nav_menu([
+            'theme_location' => 'main-menu',
+            'container' => 'nav',
+            'container_class' => 'w-full',
+            'menu_class' => 'flex justify-center gap-x-4',
+            'add_li_class' => '',
+            'link_before' => '',
+            'link_after' => '',
+        ]); ?>
+        <div id="search">
+            <?php get_search_form(); ?>
+        </div>
+    </header>
+    <main id="content">
